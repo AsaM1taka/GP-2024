1a94c886775691fae4170663389d5149a147321e
C24399391 Amy Madden <C24399391@myTUDublin.ie>
2024-09-30 16:55:09 +0100

    week3

--- the-course/sprite_2d.gd
+++ the-course/sprite_2d.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+
+@export var bullet_scene:PackedScene
+@export var bullet_spawn_point:Node2D
 const SPEED = 300.0
 const JUMP_VELOCITY= -400.0
 
@@ -11,6 +14,19 @@ func _physics_process(delta: float) -> void:
 	print(r)
 	
 	var rot = deg_to_rad(r * TURN_RATE * delta)
+	
+	var f = Input.get_axis("BACKWARDS", "FORWARD")
+	
+	var vel = transform.y * f * SPEED
+	
+	if Input.is_action_pressed("fire"):
+	var b = bullet_scene.instantiate()
+	add child(b)
+	
+	
+	velocity = vel
+	
+
 	
 	rotate(rot)
 	move_and_slide()
