7bccdac15b7f7344acd4e7cdfaf7833164fb9321
C24399391 Amy Madden <C24399391@myTUDublin.ie>
2024-10-14 16:54:34 +0100

    in process of bullet work

--- the-course/mob.gd
+++ the-course/mob.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 var play 
 
+func _ready():
+	play =  get_node("/root/Game/Player")
+
 func _physics_process(delta):
 	var direction = global_position.direction_to(play.global_position)
 	velocity = direction * 300
