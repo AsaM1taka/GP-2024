69aaa7d95270dfc393d37ca6fc42b6c27a088664
C24399391 Amy Madden <C24399391@myTUDublin.ie>
2024-11-04 14:30:38 +0000

    global position is getting an error??

--- the-course/evil.gd
+++ the-course/evil.gd
@@ -1,29 +1,35 @@
 extends Sprite2D
 
+@export var time = 5
 @export var speed = 10
-@onready var playerr = $".../playerr"
+@onready var player = $".../Playerr"
 
 func _ready() -> void:
 	
 	var a:float = 10/17
 	var b:float
-	
+
 	b = a * 17.0
 	print(b)
 	
 	pass
-
+	
+var tot_time:float = 0
 func _process(delta: float) -> void:
 	
-	var to_playerr: Vector2 = playerr.global_position - global_position
+	var to_player:Vector2 = player.global_position - global_position
 	
-	DebugDraw2D.set_text("Dist to player", to_playerr.length())
+	if to_player.length() > 0.1:
+
+		DebugDraw2D.set_text("Dist to player", to_player.length())
 	
-	to_playerr = to.playerr.normalised()
-	DebugDraw2D.set_text("NORM to player", to_playerr.length())
+		to_player = to_player.normalized()
+		DebugDraw2D.set_text("NORM to player", to_player.length())
+		
+		global_position = global_position + to_player * speed * delta
+		tot_time += delta
+		DebugDraw2D.set_text("total time", tot_time)
+		pass
 	
-	
-	
-	time = distance % speed
 	
 	
