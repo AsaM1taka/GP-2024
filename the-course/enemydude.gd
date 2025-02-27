eab1b1671eeb2a3a53ee1ebff405f93c2320b024
amy <C24399391@myTUDublin.ie>
2025-02-27 12:59:58 +0000

    enemy follows when enter area

--- the-course/enemydude.gd
+++ the-course/enemydude.gd
@@ -7,3 +7,9 @@ var attacking = false
 func _process(delta):
 	if attacking:
 		position.x = position.x - speed * delta
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "JUMPDUDE":
+		attacking = true
+	pass # Replace with function body.
