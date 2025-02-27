7ab0e557361f1f7fb1141393b18af358b4b9cbcd
amy <C24399391@myTUDublin.ie>
2025-02-27 12:57:39 +0000

    enemy

--- the-course/jumpdude.gd
+++ the-course/jumpdude.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var score = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,3 +23,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	
